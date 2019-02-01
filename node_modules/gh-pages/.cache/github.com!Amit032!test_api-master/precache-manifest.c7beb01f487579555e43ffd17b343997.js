d6547b632eb3a4c5429c42d9d3cc94468af4f875
Pratik Kalathiya <amitnit2k18@gmail.com>
2019-02-01 11:45:45 +0530

    Updates

---
+++ node_modules/gh-pages/.cache/github.com!Amit032!test_api-master/precache-manifest.c7beb01f487579555e43ffd17b343997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a9fd6dd23b6bcc412e",
+    "url": "/test_api-master/static/css/main.3706e501.chunk.css"
+  },
+  {
+    "revision": "83a9fd6dd23b6bcc412e",
+    "url": "/test_api-master/static/js/main.83a9fd6d.chunk.js"
+  },
+  {
+    "revision": "ea947d66b816256e03f5",
+    "url": "/test_api-master/static/js/1.ea947d66.chunk.js"
+  },
+  {
+    "revision": "c87aab695f2a5e9f345d",
+    "url": "/test_api-master/static/js/runtime~main.c87aab69.js"
+  },
+  {
+    "revision": "012198baa375d020f85b39de52585583",
+    "url": "/test_api-master/index.html"
+  }
+];
